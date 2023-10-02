d4c8fd99f5694efbfd8214761ba82e8a203a31e1
LandFCreative <deneschukc@yahoo.ca>
2023-10-02 15:54:33 -0600

    first commit

---
+++ tempconverter/js/main.js
@@ -0,0 +1,16 @@
+//Write your pseduo code first!
+// INput value
+document.querySelector('#yell').addEventListener('click', run)
+
+// convert temp
+function run() {
+   console.log("Hello") 
+var pumpkin = document.querySelector('#cel').value;
+pumpkin = pumpkin * 9/5 + 32;
+document.querySelector("#placeToYell").innerText = pumpkin
+}
+
+
+
+// put in browser/display
+
